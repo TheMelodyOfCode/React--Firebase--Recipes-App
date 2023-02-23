47b25a62b527990180531f05fe95bfedf8b280bd
D.Cameron <lobbygow@posteo.de>
2023-02-23 16:34:03 +0100

    testing deploy through github commits

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
+        <h2 className="title">Testinguuuuus</h2>
       </div>
     </div>
   );
