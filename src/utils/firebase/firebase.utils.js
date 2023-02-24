044816a78ccecc9b4fac7a6c7cca9d33be0d34fc
D.Cameron <lobbygow@posteo.de>
2023-02-24 17:21:43 +0100

    testing configuration

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -18,15 +18,34 @@ import {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+
+  apiKey: "AIzaSyBkqqaJrByonmoW2d5k1RqJWxdzX_18sWU",
+
+  authDomain: "recepies-64384.firebaseapp.com",
+
+  projectId: "recepies-64384",
+
+  storageBucket: "recepies-64384.appspot.com",
+
+  messagingSenderId: "84435704733",
+
+  appId: "1:84435704733:web:01404c28e5ad9b14992d74",
+
+  measurementId: "G-5B84BWCMYN"
+
 };
+
 
 
 // Initialize Firebase
