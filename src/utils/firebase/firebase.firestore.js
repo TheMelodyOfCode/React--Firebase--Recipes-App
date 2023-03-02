4d774ea3f5160d5524f4f24d8058391b10a10629
D.Cameron <lobbygow@posteo.de>
2023-03-02 14:57:24 +0100

    added firestore config

---
+++ src/utils/firebase/firebase.firestore.js
@@ -0,0 +1,13 @@
+import { 
+  getFirestore, 
+  // doc, 
+  // getDoc, 
+  // setDoc,
+  // collection,
+  // writeBatch,
+  // query,
+  // getDocs,
+} from "firebase/firestore";
+
+
+export const db = getFirestore();
