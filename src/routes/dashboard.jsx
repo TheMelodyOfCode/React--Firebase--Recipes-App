c34f69a2fc0d5be88ad6994ad307bd4935e66570
D.Cameron <lobbygow@posteo.de>
2023-04-05 23:01:12 +0200

    fix: mq issue for small mobile device

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -3,23 +3,23 @@ import * as React from 'react';
 import ItemCard from '../components/itemCards/itemCards';
 import FilterRow from '../components/filterRow/filterRow';
 import Pagination from '../components/pagination/pagination';
-import ItemCardFallback from "../components/itemCards/itemCardFallback";
-import { FirestoreContext } from '../contexts/firestore.context';
-// import HeaderCarousel from '../components/headerCarousel/headerCarousel';
+// import ItemCardFallback from "../components/itemCards/itemCardFallback";
+// import { FirestoreContext } from '../contexts/firestore.context';
+
 
 const Dashboard = () => {
 
-  const { recipes} = React.useContext(FirestoreContext);
+  // const { recipes} = React.useContext(FirestoreContext);
 
     return (
       <>
             <FilterRow />
-            {/* <HeaderCarousel /> */}
-            {
-              // !recipes.length ? <ItemCardFallback />
-              // : 
+            {/* {
+              !recipes.length ? <ItemCardFallback />
+              : 
             <ItemCard />  
-            }
+            } */}
+             <ItemCard /> 
             <Pagination />
       </>
 
