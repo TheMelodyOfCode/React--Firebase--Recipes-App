5bc088f322bb48f309b1034c38489fe7496571a2
D.Cameron <lobbygow@posteo.de>
2023-04-08 18:50:16 +0200

    fix: reverted changes

--- src/utils/firebase/firebase.auth.js
+++ src/utils/firebase/firebase.auth.js
@@ -33,19 +33,8 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const initApp = initializeApp(firebaseConfig);
 
-const { initializeAppCheck, ReCaptchaV3Provider } = require("firebase/app-check");
-// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-// key is the counterpart to the secret key you set in the Firebase console.
-export const initApp = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('6LeF_W0lAAAAAP04bOHNSp3bNpjRE6Zn9FIKUV1U'),
-
-  // Optional argument. If true, the SDK automatically refreshes App Check
-  // tokens as needed.
-  isTokenAutoRefreshEnabled: true
-});
-console.log(initApp);
 // eslint-disable-next-line no-unused-vars
 const analytics = getAnalytics(initApp);
 
