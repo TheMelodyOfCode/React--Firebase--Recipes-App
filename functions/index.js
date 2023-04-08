0cfd30c0f65b815889d98f69bd6b4ac05beb7de6
D.Cameron <lobbygow@posteo.de>
2023-04-08 17:56:05 +0200

    feat: added firebase function to app

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const functions = require("firebase-functions");
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+
+
+exports.helloWorld = functions
+    .region("europe-west1")
+    .https.onRequest((request, response) => {
+      functions.logger.info("Hello logs!", {structuredData: true});
+      response.send("Hello from Firebase!");
+    });
--- src/utils/firebase/firebase.auth.js
+++ src/utils/firebase/firebase.auth.js
@@ -31,9 +31,20 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
+const { initializeAppCheck, ReCaptchaV3Provider } = require("firebase/app-check");
 
 // Initialize Firebase
-export const initApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+export const initApp = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LeF_W0lAAAAAP04bOHNSp3bNpjRE6Zn9FIKUV1U'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
 
 // eslint-disable-next-line no-unused-vars
 const analytics = getAnalytics(initApp);
